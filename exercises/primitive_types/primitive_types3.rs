bb4124c9cf617f15ed0766d5ff16326c8fa2066b
hjbar <hjbar@protonmail.com>
2024-02-20 17:37:23 +0100

    Add rustlings

---
+++ exercises/primitive_types/primitive_types3.rs
@@ -0,0 +1,19 @@
+// primitive_types3.rs
+//
+// Create an array with at least 100 elements in it where the ??? is.
+//
+// Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
+// for a hint.
+
+const size : usize = 101;
+
+fn main() {
+       let a : [i32; size] = [0; size];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+        panic!("Array not big enough, more elements needed")
+    }
+}
